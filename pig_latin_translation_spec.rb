ea8e0d5b09ddc73506ca0d1c71bedf4e004830b6
Helen Hood <helen.c.hood@gmail.com>
2013-12-06 14:14:15 -0500

    translates words starting in consonant clusters

--- pig_latin_translation_spec.rb
+++ pig_latin_translation_spec.rb
@@ -10,7 +10,11 @@ describe PigLatinTranslator do
     expect(PigLatinTranslator.translate('piano')).to eql('ianopay')
   end
 
-  it 'translates a word starting in a consonant cluster'
+  it 'translates a word starting in a consonant cluster' do
+    expect(PigLatinTranslator.translate('string')).to eql('ingstray')
+  end
 
-  it 'returns a string containing each word of the phrase, translated'
+  it 'returns a string containing each word of the phrase, translated' do
+    expect(PigLatinTranslator.translate('animal piano string')).to eql('animalway ianopay ingstray')
+  end
 end
--- pig_latin_translator.rb
+++ pig_latin_translator.rb
@@ -1,16 +1,30 @@
 class PigLatinTranslator
   def self.translate(phrase)
-    if self.starts_with_vowel?(phrase)
-      phrase << 'way'
-    else
-      array_of_letters = phrase.split(//)
-      first_letter = array_of_letters.shift
-      array_of_letters.join << first_letter + 'ay'
-    end
+      if self.starts_with_vowel?(phrase)
+        phrase << 'way'
+      else
+        string = phrase
+        until self.starts_with_vowel?(string)
+          array_of_letters = string.split(//)
+          first_letter = array_of_letters.shift
+          string = array_of_letters.join << first_letter
+        end
+        string << 'ay'
+      end
   end
 
   private
   def self.starts_with_vowel?(word)
     word.downcase.match(/^[aeiou]/)
+  end
+
+  def self.rotate_consonant(word)
+    string = word
+    until self.starts_with_vowel?(string)
+      array_of_letters = string.split(//)
+      first_letter = array_of_letters.shift
+      string = array_of_letters.join << first_letter
+    end
+    string
   end
 end
